2cc0d85fdea5eb3098886e31b30f04b18bbf32f9
avibhardwaj233 <73737806+avibhardwaj233@users.noreply.github.com>
2022-01-13 01:39:58 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+setup(
+  name = 'Multi Function Runner',         
+  packages = ['Multi Function Runner'],   
+  version = '1.0.1',      
+  license='MIT',        
+  description = 'A simple library for Python to run multiple functions in a lambda function.',   
+  author = 'Tejas',                   
+  author_email = 'help.a.helpcenter@gmail.com',      
+  url = 'https://github.com/avibhardwaj233/Multi-Function-Runner',   
+  download_url = 'https://github.com/avibhardwaj233/Multi-Function-Runner/archive/refs/tags/1.0.1.tar.gz',    
+  keywords = ['Function', 'Multi Function runner', 'Multi function', 'Function runner'],   
+  install_requires=[],
+
+  classifiers=[
+    'Development Status :: 4 - Alpha',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+  ],
+)
